131ff7262ebee9f213e844df4e03f5da56f5ff87
jaehwanhan12 <51120983+jaehwanhan12@users.noreply.github.com>
2019-06-17 18:02:00 +0900

    Create chap3.basic_single_layer.py

---
+++ 1.TensorFlow-basic/chap3.basic_single_layer.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import  numpy as np
+
+x_data=np.array([[0,0],[1,0],[1,1],[0,0],[0,0],[0,1]])
+y_data=np.array([[1,0,0],[0,1,0],[0,0,1],[1,0,0],[1,0,0],[0,0,1]])
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+W = tf.Variable(tf.random_uniform([2,3],-1.,1.))
+b = tf.Variable(tf.zeros([3]))
+L = tf.add(tf.matmul(X,W),b)
+L = tf.nn.relu(L)
+#####soft max
+
+model = tf.nn.softmax(L)
+
+## cost function
+cost = tf.reduce_mean(-tf.reduce_sum(Y*tf.log(model),axis=1))
+
+### gradient descent
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train_op = optimizer.minimize(cost)
+#reset session
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+#train 100 times
+
+for step in range(200):
+    sess.run(train_op, feed_dict={X: x_data, Y: y_data})
+
+    if (step + 1) % 10 == 0:
+        print(step+1,sess.run(cost,feed_dict={X: x_data, Y: y_data}))
+prediction = tf.argmax(model, axis=1)
+target = tf.argmax(Y, axis=1)
+print('prediction:', sess.run(prediction,feed_dict={X: x_data}))
+print("realvalue:",sess.run(target,feed_dict={Y: y_data}))
+
+is_correct = tf.equal(prediction,target)
+accuracy = tf.reduce_mean(tf.cast(is_correct,tf.float32))
+print('accuracy: %.2f' % sess.run(accuracy * 100, feed_dict={X: x_data, Y: y_data}))
